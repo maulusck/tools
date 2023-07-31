0d63a5ca8f156348c4fe555cd94fd79bc9228cca
Maulusck <maulusck@pm.me>
2023-07-31 22:47:17 +0200

    unified two scripts

--- scripts/mikrotik-backup-ssh.sh
+++ scripts/mikrotik-backup-ssh.sh
@@ -3,13 +3,16 @@ set -e
 
 ## env vars
 MIKROTIKS="
-SA2019-RO
+mikrotik1
+mikrotik2
+mikrotik3
+...
 "
 #
-BAK_DIR="/media/varie-repartotecnico/BACKUP/mikrotik"
-#SSH_USER="admin"
-#SSH_PORT="22"
-SSH_KEY="/home/.backup/.ssh/id_rsa"
+BAK_DIR="/srv/bak/mikrotik"
+SSH_USER="admin"
+SSH_PORT="22"
+SSH_KEY="/home/.service/.ssh/id_rsa"
 ##
 
 # exit if backup folder does not exist
@@ -19,7 +22,6 @@ SSH_KEY="/home/.backup/.ssh/id_rsa"
 
 # ssh and export config to subfolder
 for mikrotik in $MIKROTIKS; do
-        mkdir -p $BAK_DIR/$mikrotik
-        #ssh  -p $SSH_PORT -i $SSH_KEY $SSH_USER@$mikrotik export > $BAK_DIR/$mikrotik/$mikrotik-export-bak-$(date -I).txt
-        ssh  -i $SSH_KEY $mikrotik export > $BAK_DIR/$mikrotik/$mikrotik-export-bak-$(date -I).txt
+	mkdir -p $BAK_DIR/$mikrotik
+	echo "ssh -p $SSH_PORT -i $SSH_KEY $SSH_USER@$mikrotik export | tee $BAK_DIR/$mikrotik/$mikrotik-export-bak-$(date -I).txt"
 done
