3d99952eb9fa2c2a0c8ffbf562885fe39cf5f9f5
Maulusck <maulusck@pm.me>
2023-08-02 01:09:52 +0200

    already enhancements...

--- scripts/vpnbook-tool.sh
+++ scripts/vpnbook-tool.sh
@@ -19,14 +19,20 @@ packages="
 	https://www.vpnbook.com/free-openvpn-account/vpnbook-openvpn-fr8.zip
 "
 
+echo	--		|v|p|n|b|o|o|k|_|t|o|o|l|		--
+
 cd $install_dir
 for package in $packages; do (
 	printf "Downloading '$package'... "
 	wget -q -c -t 0 -T 15 $package && echo "Done." || echo "Fail."
 ); done
+for package in vpnbook-*.zip; do (
+	printf "Extracting '$package'... "
+	7z e -o* $package >/dev/null && (echo "Done." ; rm -f $package) || echo "Fail."
+); done
 cd - >/dev/null
 
-echo "\nCerts downloaded in '$install_dir'."
+echo "Certs synced in '$install_dir'."
 
 tmp_file="$install_dir/password.png"
 curl -s -o $tmp_file "https://www.vpnbook.com/password.php"
