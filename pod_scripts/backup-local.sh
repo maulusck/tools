62db5cdae3b0ecbb45267fe330baccb4e6549c94
maulusck <maulusck@pm.me>
2024-02-29 22:47:12 +0100

    updated local backup script :

--- pod_scripts/backup-local.sh
+++ pod_scripts/backup-local.sh
@@ -6,4 +6,16 @@ BAK_DIR="/srv/bak/podman/local"
 
 # do backup
 test -d ${LOCAL} || exit 1
-tar cvzf ${BAK_DIR}/local-$(date -I).tgz -C $(dirname ${LOCAL}) $(basename ${LOCAL})
+
+outfile=${BAK_DIR}/$(whoami)-local-$(date -I).tar.xz
+
+# use tqdm if available
+[ -z "$(command -v tqdm)" ] \
+&& (
+        tar cJf ${outfile} -C $(dirname ${LOCAL}) $(basename ${LOCAL}) || exit 1
+) || (
+        tar cJf - -C $(dirname ${LOCAL}) $(basename ${LOCAL})\
+        | tqdm --bytes --total $(du -sb ${LOCAL}|cut -f1) > ${outfile} || exit 1
+)
+
+exit 0
