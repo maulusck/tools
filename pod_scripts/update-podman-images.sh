df81c6396df978e0651b737e9d974fc5012120e9
Maulusck <m4ulu5ck@GUND4M.ppg8.lan>
2025-07-03 22:46:43 +0200

    ..

--- pod_scripts/update-podman-images.sh
+++ pod_scripts/update-podman-images.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-for image in $(podman images --noheading | grep -v 'localhost' | awk -F " " '{print$1 ":" $2}'); do podman pull $image; done
+for image in $(podman images --noheading | grep -vE '<none>|localhost' | awk -F " " '{print$1 ":" $2}'); do podman pull $image; done
 podman image prune -f
